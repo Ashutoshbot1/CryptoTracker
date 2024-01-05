f38af7e421a0d38fe28477cc020b63220a186e79
Ashutoshbot1 <singhashutoshsa2126@gmail.com>
2024-01-05 15:43:33 +0530

    Fetching Coins Data Completed-Dashboard

--- src/Components/Dashboard/Tabs/Tabs.js
+++ src/Components/Dashboard/Tabs/Tabs.js
@@ -1,1 +1,50 @@
+import React, { useState } from "react";
+import "./Tabs.css";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { createTheme, ThemeProvider } from "@mui/material";
 
+export default function Tabs({coins}) {
+  const [value, setValue] = useState("grid");
+
+  const style = {
+    color: "var(--white)",
+    width: "50vw",
+    fontSize: "1.2rem",
+    fontWeight: 600,
+    fontFamily: "Inter",
+    textTransform: "capitalize",
+  };
+
+  // Change mui Theme
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+      },
+    },
+  });
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <TabContext value={value}>
+        <TabList variant="fullWidth" onChange={handleChange}>
+          <Tab sx={style} label="Grid" value="grid" />
+          <Tab sx={style} label="List" value="list" />
+        </TabList>
+        <TabPanel value="grid">
+            <div>mappin for grid</div>
+        </TabPanel>
+        <TabPanel value="list">
+            <div>mappin for list</div>
+        </TabPanel>
+      </TabContext>
+    </ThemeProvider>
+  );
+}
--- src/Pages/DashboardPage.js
+++ src/Pages/DashboardPage.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Tabs from '../Components/Dashboard/Tabs/Tabs'
+import Header from '../Components/Common/Header/Header'
+import axios from 'axios';
 
 const DashboardPage = () => {
+  const [coins,setCoins]=useState([]);
+
+  // Getting Coins on Page Load
+  useEffect(()=>{
+    const url="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+
+    axios.get(url)
+    .then((response)=>{
+      console.log("Coins From Dashboard",response);
+      setCoins(response.data);
+    })
+    .catch(err=>console.log("Fetching Coin error",err))
+
+  },[]) 
+
   return (
     <div>
-      
+      <Header/>
+      <Tabs coins={coins}/>
     </div>
   )
 }
